54821dce6731efde3a6d60fa33bc887a424bc37d
Mark Johnson <markjohnson@bath.edu>
2020-05-18 18:02:53 -0700

    Update security issues and publish build

---
+++ build/precache-manifest.55b5432816794869909dedecc7230689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76de6e3e64b7833ac424f62b4848813e",
+    "url": "/smart-mirror/index.html"
+  },
+  {
+    "revision": "a8221a44b9ea60b69c31",
+    "url": "/smart-mirror/static/css/main.de4615d1.chunk.css"
+  },
+  {
+    "revision": "09e6325c5d23691008a4",
+    "url": "/smart-mirror/static/js/2.6c2413c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-mirror/static/js/2.6c2413c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8221a44b9ea60b69c31",
+    "url": "/smart-mirror/static/js/main.5ba41b06.chunk.js"
+  },
+  {
+    "revision": "b9996e3f24616371f9cc",
+    "url": "/smart-mirror/static/js/runtime-main.1b579476.js"
+  }
+]);
